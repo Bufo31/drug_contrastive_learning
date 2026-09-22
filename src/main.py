3fa038af3a15f25d1721d88b06f54c4167f06b3a
Bufo31 <yp.zhang31@gmail.com>
2026-09-22 10:39:00 +0800

    Add similarity-aware loss experiments

--- src/main.py
+++ src/main.py
@@ -1,14 +1,14 @@
 from train import train
 from evaluate import evaluate
 
-epochs = 20
+epochs = 10
 batch_size = 256
 learning_rate = 1e-3
 
-mode = "train"
+mode = "2"   #   1：train   2：evaluate
 
-if mode == "train":
+if mode == "1":
     train(epochs,batch_size,learning_rate)
 
-elif mode == "evaluate":
+elif mode == "2":
     evaluate(batch_size)
